08a87828d7c0853f1bfbb4a69123349390e35777
iwai32 <iwakou32@gmail.com>
2021-10-11 01:25:14 +0900

    Enumに対応した文字列を付与するカスタム属性を作成、またそのEnumに紐づいた文字列を取得するクラスを作成

---
+++ Assets/MyGames/Scripts/Enum/GameResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+/// <summary>
+/// ゲーム結果
+/// </summary>
+public enum GameResult
+{
+    [StringValue("YOU WIN！")]
+    GAME_WIN,
+    [StringValue("DRAW！")]
+    GAME_DRAW,
+    [StringValue("YOU LOSE！")]
+    GAME_LOSE
+}
