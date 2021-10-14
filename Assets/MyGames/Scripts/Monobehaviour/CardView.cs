58083c3eb51e46906c681f9c889eea01e3ac7fab
iwai32 <iwakou32@gmail.com>
2021-10-15 05:21:06 +0900

    相手のカードは裏側で表示する

--- Assets/MyGames/Scripts/Monobehaviour/CardView.cs
+++ Assets/MyGames/Scripts/Monobehaviour/CardView.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class CardView : MonoBehaviour
 {
-    [SerializeField] Text nameText;
-    [SerializeField] Image iconImage;
+    [SerializeField] [Header("カード名")] Text nameText;
+    [SerializeField] [Header("カードの画像")] Image iconImage;
+    [SerializeField] [Header("カードの裏側")] GameObject backSide;
 
     /// <summary>
     /// カードを描画する
@@ -16,5 +17,6 @@ public class CardView : MonoBehaviour
     {
         nameText.text = cardModel.Name;
         iconImage.sprite = cardModel.Icon;
+        backSide.SetActive(!cardModel.IsPlayerCard);//相手のカードは裏側で表示する
     }
 }
