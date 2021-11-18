b22df6fdeea96c34242531e3f5fe1298f69cc4fa
iwai32 <iwakou32@gmail.com>
2021-11-18 14:35:14 +0900

    UIで共通して使用する処理をstaticクラスから呼び出すように変更

---
+++ Assets/MyGames/Scripts/Utility/UIUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using DG.Tweening;
+using UnityEngine;
+
+/// <summary>
+/// UIで使用できる共通処理を記述します
+/// </summary>
+public static class UIUtil
+{
+    /// <summary>
+    /// DOTweenでアンカーのX軸を移動します
+    /// </summary>
+    /// <param name="rectTransform"></param>
+    /// <param name="endValue"></param>
+    /// <param name="duration"></param>
+    /// <returns></returns>
+    public static Tween MoveAnchorPosXByDOT(RectTransform rectTransform, float endValue, float duration)
+    {
+        return rectTransform.DOAnchorPosX(endValue, duration);
+    }
+
+    /// <summary>
+    /// targetを画面端に移動した場合のX方向の値を取得
+    /// </summary>
+    /// <returns></returns>
+    public static float GetScreenEdgeXFor(float targetWidthX)
+    {
+        //画面端 = 画面の横幅÷2 + UIの横幅分
+        return (Screen.width / 2) + targetWidthX;
+    }
+}
