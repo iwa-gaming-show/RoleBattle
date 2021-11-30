bb2889742459e86da8b6c3684df90d22b183de42
iwai32 <iwakou32@gmail.com>
2021-12-01 08:38:03 +0900

    バトルフェーズのenumをintにキャストしたsetter,getterのカスタムプロパティを追加。こちらはenum型のままだと正しく機能しないため、intにキャストして使用するようにしました。

--- Assets/MyGames/Scripts/Extensions/RoomPropertiesExtensions.cs
+++ Assets/MyGames/Scripts/Extensions/RoomPropertiesExtensions.cs
@@ -1,12 +1,14 @@
 using ExitGames.Client.Photon;
 using Photon.Realtime;
 using static InitializationData;
+using static BattlePhase;
 
 public static class RoomPropertiesExtensions
 {
     static readonly Hashtable propsToSet = new Hashtable();
     static readonly string RoundCountKey = "RoundCount";
     static readonly string PlayerTurnCoinKey = "PlayerTurnCoin";
+    static readonly string BattlePhaseKey = "BattlePhase";
 
     /// <summary>
     /// ルームのラウンド数を取得する
@@ -48,6 +50,28 @@ public static class RoomPropertiesExtensions
     public static void SetPlayerTurnCoin(this Room room, bool playerTurnCoin)
     {
         propsToSet[PlayerTurnCoinKey] = playerTurnCoin;
+        room.SetCustomProperties(propsToSet);
+        propsToSet.Clear();
+    }
+
+    /// <summary>
+    /// バトルのフェーズを取得する
+    /// </summary>
+    /// <param name="player"></param>
+    /// <returns></returns>
+    public static int GetBattlePhase(this Room room)
+    {
+        return (room.CustomProperties[BattlePhaseKey] is int battlePhase) ? battlePhase : 0;
+    }
+
+    /// <summary>
+    /// バトルのフェーズを設定する
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="point"></param>
+    public static void SetBattlePhase(this Room room, BattlePhase battlePhase)
+    {
+        propsToSet[BattlePhaseKey] = (int)battlePhase;
         room.SetCustomProperties(propsToSet);
         propsToSet.Clear();
     }
