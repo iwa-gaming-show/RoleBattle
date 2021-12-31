9662587a5b9a8ae958a766621c304434ece31bfa
iwai32 <iwakou32@gmail.com>
2021-12-31 20:12:18 +0900

    不要になったクラスを削除

--- Assets/MyGames/Tests/EditMode/RoundDataTest.cs
+++ Assets/MyGames/Tests/EditMode/RoundDataTest.cs
@@ -1,96 +1,96 @@
-using NUnit.Framework;
+//using NUnit.Framework;
 
-public class RoundDataTest
-{
-    RoundData roundData;
+//public class RoundDataTest
+//{
+//    RoundData roundData;
 
-    [SetUp]
-    public void SetUp()
-    {
-        roundData = new RoundData(3);//3はラウンドの最大数
-    }
+//    [SetUp]
+//    public void SetUp()
+//    {
+//        roundData = new RoundData(3);//3はラウンドの最大数
+//    }
 
-    [Test, Description("プレイヤーがラウンド中にスキルを発動したかの設定")]
-    [TestCase(true, true)]
-    [TestCase(false, false)]
-    public void SetIsUsingPlayerSkillInRoundTest(bool input, bool expected)
-    {
-        //初期値はfalseになっているか
-        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.False);
+//    [Test, Description("プレイヤーがラウンド中にスキルを発動したかの設定")]
+//    [TestCase(true, true)]
+//    [TestCase(false, false)]
+//    public void SetIsUsingPlayerSkillInRoundTest(bool input, bool expected)
+//    {
+//        //初期値はfalseになっているか
+//        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.False);
 
-        roundData.SetIsUsingPlayerSkillInRound(input);
-        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.EqualTo(expected));
-    }
+//        roundData.SetIsUsingPlayerSkillInRound(input);
+//        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.EqualTo(expected));
+//    }
 
-    [Test, Description("エネミーがラウンド中にスキルを発動したかの設定")]
-    [TestCase(true, true)]
-    [TestCase(false, false)]
-    public void SetIsUsingEnemySkillInRoundTest(bool input, bool expected)
-    {
-        //初期値はfalseになっているか
-        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.False);
+//    [Test, Description("エネミーがラウンド中にスキルを発動したかの設定")]
+//    [TestCase(true, true)]
+//    [TestCase(false, false)]
+//    public void SetIsUsingEnemySkillInRoundTest(bool input, bool expected)
+//    {
+//        //初期値はfalseになっているか
+//        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.False);
 
-        roundData.SetIsUsingPlayerSkillInRound(input);
-        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.EqualTo(expected));
-    }
+//        roundData.SetIsUsingPlayerSkillInRound(input);
+//        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.EqualTo(expected));
+//    }
 
-    [Test, Description("ラウンドの設定")]
-    [TestCase(0, 1)]
-    [TestCase(2, 2)]
-    [TestCase(-1, 1)]
-    [TestCase(9999, 3)]
-    public void SetRoundCountTest(int input, int expected)
-    {
-        roundData.SetRoundCount(input);
-        Assert.That(roundData.RoundCount, Is.EqualTo(expected));
-    }
+//    [Test, Description("ラウンドの設定")]
+//    [TestCase(0, 1)]
+//    [TestCase(2, 2)]
+//    [TestCase(-1, 1)]
+//    [TestCase(9999, 3)]
+//    public void SetRoundCountTest(int input, int expected)
+//    {
+//        roundData.SetRoundCount(input);
+//        Assert.That(roundData.RoundCount, Is.EqualTo(expected));
+//    }
 
-    [Test, Description("ラウンドの加算がされているか")]
-    [TestCase(1, 2, 3)]
-    [TestCase(-1, 2, 3)]
-    [TestCase(9999, 4, 5)]
-    public void AddRoundCountTest(int input, int expected1, int expected2)
-    {
-        roundData.SetRoundCount(input);
-        roundData.AddRoundCount();
-        Assert.That(roundData.RoundCount, Is.EqualTo(expected1));
+//    [Test, Description("ラウンドの加算がされているか")]
+//    [TestCase(1, 2, 3)]
+//    [TestCase(-1, 2, 3)]
+//    [TestCase(9999, 4, 5)]
+//    public void AddRoundCountTest(int input, int expected1, int expected2)
+//    {
+//        roundData.SetRoundCount(input);
+//        roundData.AddRoundCount();
+//        Assert.That(roundData.RoundCount, Is.EqualTo(expected1));
 
-        //連続して呼び出して問題なく加算されているか
-        roundData.AddRoundCount();
-        Assert.That(roundData.RoundCount, Is.EqualTo(expected2));
-    }
+//        //連続して呼び出して問題なく加算されているか
+//        roundData.AddRoundCount();
+//        Assert.That(roundData.RoundCount, Is.EqualTo(expected2));
+//    }
 
-    [Test, Description("ラウンドの状態が正しくリセットされているか")]
-    public void ResetRoundStateTest()
-    {
-        roundData.ResetRoundState();
-        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.False);
-        Assert.That(roundData.IsUsingEnemySkillInRound, Is.False);
-    }
+//    [Test, Description("ラウンドの状態が正しくリセットされているか")]
+//    public void ResetRoundStateTest()
+//    {
+//        roundData.ResetRoundState();
+//        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.False);
+//        Assert.That(roundData.IsUsingEnemySkillInRound, Is.False);
+//    }
 
-    [Test, Description("プレイヤーが必殺技を使用したラウンドかどうか正しく設定されているか")]
-    [TestCase(true, true, true)]
-    [TestCase(true, false, false)]
-    public void SetUsingSkillRoundByPlayerTest(bool isPlayer, bool isUsingSkill, bool expected)
-    {
-        roundData.SetUsingSkillRound(isPlayer, isUsingSkill);
+//    [Test, Description("プレイヤーが必殺技を使用したラウンドかどうか正しく設定されているか")]
+//    [TestCase(true, true, true)]
+//    [TestCase(true, false, false)]
+//    public void SetUsingSkillRoundByPlayerTest(bool isPlayer, bool isUsingSkill, bool expected)
+//    {
+//        roundData.SetUsingSkillRound(isPlayer, isUsingSkill);
 
-        //プレイヤーの場合
-        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.EqualTo(expected));
-        //エネミーは常にfalseになる
-        Assert.That(roundData.IsUsingEnemySkillInRound, Is.False);
-    }
+//        //プレイヤーの場合
+//        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.EqualTo(expected));
+//        //エネミーは常にfalseになる
+//        Assert.That(roundData.IsUsingEnemySkillInRound, Is.False);
+//    }
 
-    [Test, Description("エネミーが必殺技を使用したラウンドかどうか正しく設定されているか")]
-    [TestCase(false, true, true)]
-    [TestCase(false, false, false)]
-    public void SetUsingSkillRoundByEnemyTest(bool isPlayer, bool isUsingSkill, bool expected)
-    {
-        roundData.SetUsingSkillRound(isPlayer, isUsingSkill);
+//    [Test, Description("エネミーが必殺技を使用したラウンドかどうか正しく設定されているか")]
+//    [TestCase(false, true, true)]
+//    [TestCase(false, false, false)]
+//    public void SetUsingSkillRoundByEnemyTest(bool isPlayer, bool isUsingSkill, bool expected)
+//    {
+//        roundData.SetUsingSkillRound(isPlayer, isUsingSkill);
 
-        //エネミーの場合
-        Assert.That(roundData.IsUsingEnemySkillInRound, Is.EqualTo(expected));
-        //プレイヤーは常にfalseになる
-        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.False);
-    }
-}
+//        //エネミーの場合
+//        Assert.That(roundData.IsUsingEnemySkillInRound, Is.EqualTo(expected));
+//        //プレイヤーは常にfalseになる
+//        Assert.That(roundData.IsUsingPlayerSkillInRound, Is.False);
+//    }
+//}
--- Assets/MyGames/Tests/EditMode/TurnDataTest.cs
+++ Assets/MyGames/Tests/EditMode/TurnDataTest.cs
@@ -1,87 +1,87 @@
-using NUnit.Framework;
+//using NUnit.Framework;
 
-public class TurnDataTest
-{
-    TurnData turnData;
+//public class TurnDataTest
+//{
+//    TurnData turnData;
 
-    [SetUp]
-    public void SetUp()
-    {
-        turnData = new TurnData();
-    }
+//    [SetUp]
+//    public void SetUp()
+//    {
+//        turnData = new TurnData();
+//    }
 
-    [Test, Description("自身のターンかの設定が正しくできているか")]
-    [TestCase(true, true)]
-    [TestCase(false, false)]
-    public void SetIsMyTurnTest(bool input, bool expected)
-    {
-        //初期値はfalseになっているか
-        Assert.That(turnData.IsMyTurn, Is.False);
+//    [Test, Description("自身のターンかの設定が正しくできているか")]
+//    [TestCase(true, true)]
+//    [TestCase(false, false)]
+//    public void SetIsMyTurnTest(bool input, bool expected)
+//    {
+//        //初期値はfalseになっているか
+//        Assert.That(turnData.IsMyTurn, Is.False);
 
-        turnData.SetIsMyTurn(input);
-        Assert.That(turnData.IsMyTurn, Is.EqualTo(expected));
-    }
+//        turnData.SetIsMyTurn(input);
+//        Assert.That(turnData.IsMyTurn, Is.EqualTo(expected));
+//    }
 
-    [Test, Description("エネミーが必殺技を使用するターンの設定が正しくできているか")]
-    [TestCase(1, 1)]
-    [TestCase(2, 2)]
-    public void SetEnemySpSkillTurnTest(int input, int expected)
-    {
-        turnData.SetEnemySpSkillTurn(input);
-        Assert.That(turnData.EnemySpSkillTurn, Is.EqualTo(expected));
-    }
+//    [Test, Description("エネミーが必殺技を使用するターンの設定が正しくできているか")]
+//    [TestCase(1, 1)]
+//    [TestCase(2, 2)]
+//    public void SetEnemySpSkillTurnTest(int input, int expected)
+//    {
+//        turnData.SetEnemySpSkillTurn(input);
+//        Assert.That(turnData.EnemySpSkillTurn, Is.EqualTo(expected));
+//    }
 
-    [Test, Description("自身のターンが終わったか")]
-    [TestCase(true, true)]
-    [TestCase(false, false)]
-    public void SetIsMyTurnEndTest(bool input, bool expected)
-    {
-        //初期値はfalseになっているか
-        Assert.That(turnData.IsMyTurnEnd, Is.False);
+//    [Test, Description("自身のターンが終わったか")]
+//    [TestCase(true, true)]
+//    [TestCase(false, false)]
+//    public void SetIsMyTurnEndTest(bool input, bool expected)
+//    {
+//        //初期値はfalseになっているか
+//        Assert.That(turnData.IsMyTurnEnd, Is.False);
 
-        turnData.SetIsMyTurnEnd(input);
-        Assert.That(turnData.IsMyTurnEnd, Is.EqualTo(expected));
-    }
+//        turnData.SetIsMyTurnEnd(input);
+//        Assert.That(turnData.IsMyTurnEnd, Is.EqualTo(expected));
+//    }
 
-    [Test, Description("エネミーのターンが終わったか")]
-    [TestCase(true, true)]
-    [TestCase(false, false)]
-    public void SetIsEnemyTurnEnd(bool input, bool expected)
-    {
-        //初期値はfalseになっているか
-        Assert.That(turnData.IsEnemyTurnEnd, Is.False);
+//    [Test, Description("エネミーのターンが終わったか")]
+//    [TestCase(true, true)]
+//    [TestCase(false, false)]
+//    public void SetIsEnemyTurnEnd(bool input, bool expected)
+//    {
+//        //初期値はfalseになっているか
+//        Assert.That(turnData.IsEnemyTurnEnd, Is.False);
 
-        turnData.SetIsEnemyTurnEnd(input);
-        Assert.That(turnData.IsEnemyTurnEnd, Is.EqualTo(expected));
-    }
+//        turnData.SetIsEnemyTurnEnd(input);
+//        Assert.That(turnData.IsEnemyTurnEnd, Is.EqualTo(expected));
+//    }
 
-    [Test, Description("ターンの終了フラグがリセットされているか")]
-    public void ResetTurnTest()
-    {
-        //まずは終了フラグをtrueにする
-        turnData.SetIsMyTurnEnd(true);
-        turnData.SetIsEnemyTurnEnd(true);
-        Assert.That(turnData.IsMyTurnEnd, Is.True);
-        Assert.That(turnData.IsEnemyTurnEnd, Is.True);
+//    [Test, Description("ターンの終了フラグがリセットされているか")]
+//    public void ResetTurnTest()
+//    {
+//        //まずは終了フラグをtrueにする
+//        turnData.SetIsMyTurnEnd(true);
+//        turnData.SetIsEnemyTurnEnd(true);
+//        Assert.That(turnData.IsMyTurnEnd, Is.True);
+//        Assert.That(turnData.IsEnemyTurnEnd, Is.True);
 
-        //リセットする
-        turnData.ResetTurn();
-        Assert.That(turnData.IsMyTurnEnd, Is.False);
-        Assert.That(turnData.IsEnemyTurnEnd, Is.False);
-    }
+//        //リセットする
+//        turnData.ResetTurn();
+//        Assert.That(turnData.IsMyTurnEnd, Is.False);
+//        Assert.That(turnData.IsEnemyTurnEnd, Is.False);
+//    }
 
-    [Test, Description("ターンの設定を正しく切り替えられているか")]
-    [TestCase(true, false, true, false)]
-    [TestCase(false, true, false, true)]
-    public void ChangeTurnSettingsTest(bool input, bool expected1, bool expected2, bool expected3)
-    {
-        turnData.SetIsMyTurn(input);
-        turnData.ChangeTurnSettings();
+//    [Test, Description("ターンの設定を正しく切り替えられているか")]
+//    [TestCase(true, false, true, false)]
+//    [TestCase(false, true, false, true)]
+//    public void ChangeTurnSettingsTest(bool input, bool expected1, bool expected2, bool expected3)
+//    {
+//        turnData.SetIsMyTurn(input);
+//        turnData.ChangeTurnSettings();
 
-        //自身のターンの時はIsMyTurnがfalse, IsMyTurnEndがtrue, IsEnemyTurnEndがfalse
-        //相手のターンの時はIsMyTurnがtrue, IsMyTurnEndがfalse, IsEnemyTurnEndがtrue
-        Assert.That(turnData.IsMyTurn, Is.EqualTo(expected1));
-        Assert.That(turnData.IsMyTurnEnd, Is.EqualTo(expected2));
-        Assert.That(turnData.IsEnemyTurnEnd, Is.EqualTo(expected3));
-    }
-}
+//        //自身のターンの時はIsMyTurnがfalse, IsMyTurnEndがtrue, IsEnemyTurnEndがfalse
+//        //相手のターンの時はIsMyTurnがtrue, IsMyTurnEndがfalse, IsEnemyTurnEndがtrue
+//        Assert.That(turnData.IsMyTurn, Is.EqualTo(expected1));
+//        Assert.That(turnData.IsMyTurnEnd, Is.EqualTo(expected2));
+//        Assert.That(turnData.IsEnemyTurnEnd, Is.EqualTo(expected3));
+//    }
+//}
